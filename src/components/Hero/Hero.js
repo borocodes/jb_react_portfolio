c2b5641af9f3dcb5af2fd95ac5a9169c8c05bd9b
Jonathan Borofsky <jonborofsky@gmail.com>
2022-02-17 14:52:34 -0600

    Hero completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,25 @@
+//TODO: add resume download button
+
 import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
+
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Jon<br />
+        I'm glad you're here
+      </SectionTitle>
+      <SectionText>
+      Lucas ipsum dolor sit amet ackbar wicket vader solo moff cade bothan han kashyyyk binks. Solo jar wampa dagobah tatooine coruscant owen jade owen. Qui-gon organa calamari leia.
+      </SectionText>
+      <Button>Get in touch</Button> 
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
